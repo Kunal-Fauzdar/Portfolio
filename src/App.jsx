9db7113798e0df667eb5a114063cd42deb052b94
Kunal Fauzdar <kunalfauzdar4@gmail.com>
2025-12-01 20:39:29 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from "@components/Navbar.jsx";
+
+function App() {
+  return (
+    <main>
+      <Navbar />
+    </main>
+  )
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,35 @@
+import dayjs from "dayjs";
+import { navLinks , navIcons } from "@constants";
+function Navbar(){
+  return (
+    <nav>
+      <div>
+        <img src="/images/logo.svg" alt="Logo" />
+        <p className="font-bold">Kunal's Portfolio</p>
+        <ul>
+          {navLinks.map(({id,name}) => (
+            <li key={id}>
+              <p>{name}</p>
+            </li>
+            ))
+          }
+        </ul>  
+      </div>
+      
+      <div>
+        <ul>
+          {
+            navIcons.map(({id,img})=>(
+              <li key={id}>
+                <img src={img} className="icon-hover" alt={`icon-${id}`} />
+              </li>
+            ))
+          }
+        </ul>
+        <time>{dayjs().format("ddd D MMM h:mm A")}</time>
+      </div>
+      
+    </nav>
+  );
+}
+export default Navbar;
